811dca3b873559faae02f478c425ae4b6ecdc841
Chantal-laure KALA-LOBE <chkala-l@made-f0Dr9s1.clusters.42paris.fr>
2023-09-14 19:25:44 +0200

    a

--- server.c
+++ server.c
@@ -6,7 +6,38 @@
 /*   By: chkala-l <chkala-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:38:53 by chkala-l          #+#    #+#             */
-/*   Updated: 2023/09/12 17:38:53 by chkala-l         ###   ########.fr       */
+/*   Updated: 2023/09/14 19:07:16 by chkala-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+char	*ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (str);
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t i;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_putstr("error: nb argument incorrect\n");
+		return (1);
+	}
+	i = getpid();
+	printf("le PID du server est: %d \n", i);
+	return (0);
+}
